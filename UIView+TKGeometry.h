dddb250ac8e921b78cc0cd50876633cd33719f57
Tomek Kuzma <mapedd@sezamkowa.net>
2012-09-22 00:48:55 +0200

    init

---
+++ UIView+TKGeometry.h
@@ -0,0 +1,77 @@
+//
+//  UIView+TKGeometry.h
+//  Pure
+//
+//  Created by Tomasz Kuźma on 7/27/12.
+//  Copyright (c) 2012 Creadhoc Sp. z.o.o www.creadhoc.pl  , Tomasz Kuzma (mapedd@mapedd.com). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+CGRect TKCGRectMakeBoundsFromFrame(CGRect frame);
+CGRect TKCGRectAtCenterWithSize(CGPoint center, CGSize size);
+
+@interface UIView (TKGeometry)
+
+// origin
+
+- (CGPoint)origin;
+
+- (void)setOrigin:(CGPoint)origin;
+
+// size
+
+- (CGSize)size;
+
+- (void)setSize:(CGSize)size;
+
+// frame
+
+- (void)setHeight:(CGFloat)height;
+
+- (CGFloat)height;
+
+- (void)setWidth:(CGFloat)width;
+
+- (CGFloat)width;
+
+- (CGFloat)xOrigin;
+
+- (void)setXOrigin:(CGFloat)xOrigin;
+
+- (CGFloat)yOrigin;
+
+- (void)setYOrigin:(CGFloat)yOrigin;
+
+- (void)setMaxX:(CGFloat)maxX;
+
+- (CGFloat)maxX;
+
+- (void)setMaxY:(CGFloat)maxY;
+
+- (CGFloat)maxY;
+
+// center
+
+- (CGFloat)xCenter;
+
+- (void)setXCenter:(CGFloat)xCenter;
+
+- (CGFloat)yCenter;
+
+- (void)setYCenter:(CGFloat)yCenter;
+
+@end
+
+@interface UIScrollView (TKGeometry)
+
+- (void)setContentOffsetX:(CGFloat)contentOffsetX;
+
+- (void)setContentOffsetY:(CGFloat)contentOffsetY;
+
+- (CGFloat)contentOffsetX;
+
+- (CGFloat)contentOffsetY;
+
+@end
---
+++ UIView+TKGeometry.m
@@ -0,0 +1,158 @@
+//
+//  UIView+TKGeometry.m
+//  Pure
+//
+//  Created by Tomasz Kuźma on 7/27/12.
+//  Copyright (c) 2012 Creadhoc Sp. z.o.o www.creadhoc.pl  , Tomasz Kuzma (mapedd@mapedd.com). All rights reserved.
+//
+
+#import "UIView+TKGeometry.h"
+
+
+CGRect TKCGRectMakeBoundsFromFrame(CGRect frame){
+    return CGRectMake(0.0f, 0.0f, frame.size.width, frame.size.height);
+}
+
+CGRect TKCGRectAtCenterWithSize(CGPoint center, CGSize size)
+{
+    return CGRectMake(center.x - size.width/2.0, center.y - size.height/2.0, size.width, size.height);
+}
+
+@implementation UIView (TKGeometry)
+
+
+// origin
+
+- (CGPoint)origin{
+    return self.frame.origin;
+}
+
+- (void)setOrigin:(CGPoint)origin{
+    CGRect frame = self.frame;
+    frame.origin = origin;
+    self.frame = frame;
+}
+
+// size
+
+- (CGSize)size{
+    return self.frame.size;
+}
+
+- (void)setSize:(CGSize)size{
+    CGRect frame = self.frame;
+    frame.size = size;
+    self.frame = frame;
+    
+}
+
+// frame
+
+- (CGFloat)height{
+    return self.frame.size.height;
+}
+
+- (void)setHeight:(CGFloat)height{
+    CGRect frame = self.frame;
+    frame.size.height = height;
+    self.frame = frame;
+}
+
+- (CGFloat)width{
+    return self.frame.size.width;
+}
+
+- (void)setWidth:(CGFloat)width{
+    CGRect frame = self.frame;
+    frame.size.width = width;
+    self.frame = frame;
+}
+
+- (CGFloat)xOrigin{
+    return self.frame.origin.x;
+}
+
+- (void)setXOrigin:(CGFloat)xOrigin{
+    
+    CGRect frame = self.frame;
+    frame.origin.x = xOrigin;
+    self.frame = frame;
+}
+
+- (CGFloat)yOrigin{
+    return self.frame.origin.y;
+}
+
+- (void)setYOrigin:(CGFloat)yOrigin{
+    CGRect frame = self.frame;
+    frame.origin.y = yOrigin;
+    self.frame = frame;
+}
+
+- (void)setMaxX:(CGFloat)maxX{
+    CGRect frame = self.frame;
+    frame.origin.x = maxX - frame.size.width;
+    self.frame = frame;
+}
+
+- (CGFloat)maxX{
+    return CGRectGetMaxX(self.frame);
+}
+
+- (void)setMaxY:(CGFloat)maxY{
+    CGRect frame = self.frame;
+    frame.origin.y = maxY - frame.size.height;
+    self.frame = frame;
+}
+
+- (CGFloat)maxY{
+    return CGRectGetMaxY(self.frame);
+}
+
+// center
+
+- (CGFloat)xCenter{
+    return self.center.x;
+}
+
+- (void)setXCenter:(CGFloat)xCenter{
+    CGPoint center = self.center;
+    center.x = xCenter;
+    self.center = center;
+}
+
+- (CGFloat)yCenter{
+    return self.center.y;
+}
+
+- (void)setYCenter:(CGFloat)yCenter{
+    CGPoint center = self.center;
+    center.y = yCenter;
+    self.center = center;
+}
+
+@end
+
+@implementation UIScrollView (TKGeometry)
+
+- (void)setContentOffsetX:(CGFloat)contentOffsetX{
+    CGPoint newContentOffset = self.contentOffset;
+    newContentOffset.x = contentOffsetX;
+    self.contentOffset = newContentOffset;
+}
+
+- (void)setContentOffsetY:(CGFloat)contentOffsetY{
+    CGPoint newContentOffset = self.contentOffset;
+    newContentOffset.y = contentOffsetY;
+    self.contentOffset = newContentOffset;
+}
+
+- (CGFloat)contentOffsetX{
+    return self.contentOffset.x;
+}
+
+- (CGFloat)contentOffsetY{
+    return self.contentOffset.y;
+}
+
+@end
